0c95c30f279cffa3d8ecec1e9e4cb3ac84c3d28a
Marios Vl <marios.vlachogiannis@gmail.com>
2025-02-25 15:29:10 +0200

    Add

---
+++ BCSP2024/SourceCode/generate_Inputs_Tries.c
@@ -0,0 +1,113 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int m,k,p,l,i,n,o,q,r;
+   p=1;
+   k=3;
+   l=6;
+   m=9;
+   n=12;
+   o=16;
+   q=20;
+   r=25;
+
+
+
+    for( i=1;i<=15;i++)
+    {
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4H_%d.txt \n",p,i,p,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DD_%d.txt \n",p,i,p,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4W_%d.txt \n",p,i,p,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DP_%d.txt \n",p,i,p,i);
+
+
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4H_%d.txt \n",k,i,k,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DD_%d.txt \n",k,i,k,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4W_%d.txt \n",k,i,k,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DP_%d.txt \n",k,i,k,i);
+
+    printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4H_%d.txt \n",l,i,l,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DD_%d.txt \n",l,i,l,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4W_%d.txt \n",l,i,l,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DP_%d.txt \n",l,i,l,i);
+
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4H_%d.txt \n",m,i,m,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DD_%d.txt \n",m,i,m,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4W_%d.txt \n",m,i,m,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DP_%d.txt \n",m,i,m,i);
+
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4H_%d.txt \n",n,i,n,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DD_%d.txt \n",n,i,n,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4W_%d.txt \n",n,i,n,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DP_%d.txt \n",n,i,n,i);
+
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4H_%d.txt \n",o,i,o,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DD_%d.txt \n",o,i,o,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4W_%d.txt \n",o,i,o,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DP_%d.txt \n",o,i,o,i);
+
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4H_%d.txt \n",q,i,q,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DD_%d.txt \n",q,i,q,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4W_%d.txt \n",q,i,q,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DP_%d.txt \n",q,i,q,i);
+
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4H_%d.txt \n",r,i,r,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DD_%d.txt \n",r,i,r,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4W_%d.txt \n",r,i,r,i);
+     printf(".\\bcspPlus.exe test%dX4_%d.txt out%dX4DP_%d.txt \n",r,i,r,i);
+
+
+
+
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5H_%d.txt \n",p,i,p,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DD_%d.txt \n",p,i,p,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5W_%d.txt \n",p,i,p,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DP_%d.txt \n",p,i,p,i);
+
+
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5H_%d.txt \n",k,i,k,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DD_%d.txt \n",k,i,k,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5W_%d.txt \n",k,i,k,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DP_%d.txt \n",k,i,k,i);
+
+    printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5H_%d.txt \n",l,i,l,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DD_%d.txt \n",l,i,l,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5W_%d.txt \n",l,i,l,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DP_%d.txt \n",l,i,l,i);
+
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5H_%d.txt \n",m,i,m,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DD_%d.txt \n",m,i,m,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5W_%d.txt \n",m,i,m,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DP_%d.txt \n",m,i,m,i);
+
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5H_%d.txt \n",n,i,n,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DD_%d.txt \n",n,i,n,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5W_%d.txt \n",n,i,n,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DP_%d.txt \n",n,i,n,i);
+
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5H_%d.txt \n",o,i,o,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DD_%d.txt \n",o,i,o,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5W_%d.txt \n",o,i,o,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DP_%d.txt \n",o,i,o,i);
+
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5H_%d.txt \n",q,i,q,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DD_%d.txt \n",q,i,q,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5W_%d.txt \n",q,i,q,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DP_%d.txt \n",q,i,q,i);
+
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5H_%d.txt \n",r,i,r,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DD_%d.txt \n",r,i,r,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5W_%d.txt \n",r,i,r,i);
+     printf(".\\bcspPlus.exe test%dX5_%d.txt out%dX5DP_%d.txt \n",r,i,r,i);
+
+
+
+
+    }
+
+
+    return 0;
+
+
+}
